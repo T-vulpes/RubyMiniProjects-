ebb1dcbe2ad75834a331ca8be2590a0965fdd8cf
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-06-13 15:53:27 +0300

    03

--- 03_grade_converter/grade_converter_Case.rb
+++ 03_grade_converter/grade_converter_Case.rb
@@ -1,27 +1,27 @@
-print "Vize notunuzu giriniz: "
-vize = gets.to_f
+print "Enter your midterm score: "
+midterm = gets.to_f
 
-print "Final notunuzu giriniz: "
+print "Enter your final exam score: "
 final = gets.to_f 
-ort = (vize * 0.4) + (final * 0.6)
+ort = (midterm * 0.4) + (final * 0.6)
 
-puts "Ortalamanız: #{ort.round(2)}"
+puts "Average: #{ort.round(2)}"
 
 case ort
 when 85..100
-  puts "Tebrikler! Harf notunuz: AA"
+puts "Congratulations! Your letter grade: AA"
 when 70...85
-  puts "Başarılısınız. Harf notunuz: BA"
+puts "You are successful. Your letter grade: BA"
 when 65...70
-  puts "İyi bir sonuç. Harf notunuz: BB"
+puts "A good result. Your letter grade: BB"
 when 55...65
-  puts "Ortalamanın biraz üzerinde. Harf notunuz: CB"
+puts "Slightly above average. Your letter grade: CB"
 when 45...55
-  puts "Geçtiniz. Harf notunuz: CC"
+puts "You passed. Your letter grade: CC"
 when 30...45
-  puts "Koşullu geçtiniz. Harf notunuz: DC"
+puts "You passed conditionally. Your letter grade: DC"
 when 0...30
-  puts "Maalesef kaldınız. Harf notunuz: FF"
+puts "Unfortunately, you failed. Your letter grade: FF"
 else
-  puts "Geçersiz not girişi!"
+puts "Invalid grade entry!"
 end
