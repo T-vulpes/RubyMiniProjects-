e2fd6d4605e25467a98869675e24c3364f177c56
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-06-13 14:44:04 +0300

    03_calculator_functions.rb

--- 01_calculator/03_calculator_functions.rb
+++ 01_calculator/03_calculator_functions.rb
@@ -1,52 +1,52 @@
 # calculator_functions.rb
 
-def toplam(a, b)
+def add(a, b)
   a + b
 end
 
-def cikarma(a, b)
+def subtract(a, b)
   a - b
 end
 
-def carpma(a, b)
+def multiply(a, b)
   a * b
 end
 
-def bolme(a, b)
-  return "Bir sayı sıfıra bölünemez!" if b == 0
+def divide(a, b)
+  return "Cannot divide by zero!" if b == 0
   a / b
 end
 
-def us_alma(a, b)
+def power(a, b)
   a ** b
 end
 
-def mod_alma(a, b)
+def modulo(a, b)
   a % b
 end
 
-puts "Birinci sayıyı giriniz:"
-sayi1 = gets.to_f
+puts "Enter the first number:"
+num1 = gets.to_f
 
-puts "İkinci sayıyı giriniz:"
-sayi2 = gets.to_f
+puts "Enter the second number:"
+num2 = gets.to_f
 
-puts "Hangi işlemin yapılmasını istersiniz? (+, -, /, *, **, %):"
-islem = gets.chomp.strip
+puts "Which operation would you like to perform? (+, -, /, *, **, %):"
+operation = gets.chomp.strip
 
-case islem
+case operation
 when "+"
-  puts "Sonuç: #{toplam(sayi1, sayi2)}"
+  puts "Result: #{add(num1, num2)}"
 when "-"
-  puts "Sonuç: #{cikarma(sayi1, sayi2)}"
+  puts "Result: #{subtract(num1, num2)}"
 when "*"
-  puts "Sonuç: #{carpma(sayi1, sayi2)}"
+  puts "Result: #{multiply(num1, num2)}"
 when "/"
-  puts "Sonuç: #{bolme(sayi1, sayi2)}"
+  puts "Result: #{divide(num1, num2)}"
 when "**"
-  puts "Sonuç: #{us_alma(sayi1, sayi2)}"
+  puts "Result: #{power(num1, num2)}"
 when "%"
-  puts "Sonuç: #{mod_alma(sayi1, sayi2)}"
+  puts "Result: #{modulo(num1, num2)}"
 else
-  puts "Yanlış işlem komutu girdiniz!"
+  puts "Invalid operation!"
 end
