5dfc733c316bb28915ac87f7999ff1648693e85d
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-06-21 15:58:31 +0300

    12

---
+++ 12_secure-password-generator/main.rb
@@ -0,0 +1,46 @@
+=begin
+Advanced Secure Password Generator
+Author: Deniz
+Generates a random password including at least:
+- one lowercase letter
+- one uppercase letter
+- one digit
+- one symbol
+=end
+
+require 'securerandom'
+
+# Character groups
+lowercase = ('a'..'z').to_a
+uppercase = ('A'..'Z').to_a
+digits    = ('0'..'9').to_a
+symbols   = ['!', '-', '_', '/', '=', '^', '+', '@', '%', '&', '(', ')', '{', '}', '[', ']']
+
+# Combined pool
+all_chars = lowercase + uppercase + digits + symbols
+
+# Get desired length
+puts "Enter password length (minimum 8):"
+length = gets.chomp.to_i
+
+if length < 8
+  puts "Password must be at least 8 characters."
+  exit
+end
+
+# Ensure at least one from each group
+password = []
+password << lowercase.sample
+password << uppercase.sample
+password << digits.sample
+password << symbols.sample
+
+# Fill the rest with random characters
+remaining = length - password.length
+remaining.times { password << all_chars.sample }
+
+# Shuffle for randomness
+password.shuffle!
+
+# Output the password
+puts "Generated Secure Password: #{password.join}"
